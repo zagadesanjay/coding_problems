22b503b0eb438e7aebbb4bbc1369879a11f0269e
Sanjay Zagade <2520982+zagadesanjay@users.noreply.github.com>
2021-03-06 09:54:36 +0530

    stair case height

---
+++ coding-problems-java/src/com/sz/cp/StairCaseHeight.java
@@ -0,0 +1,25 @@
+package com.sz.cp;
+
+public class StairCaseHeight {
+
+	public static void main(String[] args) {
+		System.out.println("Height = " + height(14));
+	}
+
+	public static int height(int blocks) {
+		int num = 0;
+		int i = 1;
+		int cnt = 0;
+
+		while (num <= blocks) {
+			num = num + i;
+			System.out.println(num);
+			i++;
+			cnt++;
+
+		}
+		cnt--;
+		return cnt;
+	}
+
+}
